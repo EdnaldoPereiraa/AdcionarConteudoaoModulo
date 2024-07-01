651b6d2b4014d473e0cc9475f0a0244523e84c55
Ednaldo Pereira <141043267+EdnaldoPereiraa@users.noreply.github.com>
2024-07-01 10:42:32 -0300

    ..
    adicionado botões e tags

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleOptions() {
+    var optionsContainer = document.getElementById('options-container');
+    if (optionsContainer.style.display === 'block') {
+        optionsContainer.style.display = 'none';
+    } else {
+        optionsContainer.style.display = 'block';
+    }
+}
