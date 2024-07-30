04ee7f33f9ae485b8aad712253a2196631e8c71d
Ednaldo Pereira <141043267+EdnaldoPereiraa@users.noreply.github.com>
2024-07-29 22:39:06 -0300

    tags e js
    correção das tags, cores e adc js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,17 @@ function toggleOptions() {
         optionsContainer.style.display = 'block';
     }
 }
+
+function toggleOptions() {
+    const optionsContainer = document.getElementById('options-container');
+    const arrow = document.querySelector('.tags-button .arrow');
+    
+    // Alternar a visibilidade das opções
+    if (optionsContainer.style.display === 'none' || optionsContainer.style.display === '') {
+        optionsContainer.style.display = 'block';
+        arrow.classList.add('active');
+    } else {
+        optionsContainer.style.display = 'none';
+        arrow.classList.remove('active');
+    }
+}
